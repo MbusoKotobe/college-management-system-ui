d3f0f0490af66a1369a9c0ba1002d7156fa20d8e
Mbuso Kotobe <mbusokotobewrk@gmail.com>
2022-09-30 23:13:48 +0200

    PROJECT UPDATE: Added logo to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacultyComponent } from './faculty/faculty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FacultyComponent },
+  { path: 'faculty', component: FacultyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
